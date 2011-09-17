a3dc5bb39367ec47426c78aeeb2472305bf04940
Kaloian Doganov <kaloian@doganov.org>
2011-09-17 22:31:33 +0300

    Exercise 1.3

---
+++ ex-1-3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* Отпечатва таблица по Фаренхайт и Целзий
+   за fahr = 0, 20 ... 300; версия с числа с плаваща запетая */
+
+main()
+{
+  float fahr, celsius;
+  float lower, upper, step;
+
+  lower = 0; /* долна граница на температурната таблица */
+  upper = 300; /* горна граница */
+  step = 20; /* големина на стъпката */
+
+  printf("Упражнение 1.3. Температури по Фаренхайт и Целзий\n\n");
+  printf("°C \t  °F\n");
+  printf("---\t------\n");
+
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f\t%6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+}
